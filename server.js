37b16807fee4c8189c835aa3c85a09ac369af844
Mukherjee <pmukhe00@emea.mrshmc.com>
2019-02-21 13:12:45 +0000

    made changes to deploy on heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
-
+var port = process.env.port || 3000
 
 
 app.use(express.static(__dirname+'/public'));
@@ -47,4 +47,6 @@ app.get('/bad',(req,res)=>{
     })
 })
 
-app.listen(3000);
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
+});
