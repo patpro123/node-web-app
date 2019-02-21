73d76c253a4ec0ecf875002a4348c1652a09f10c
Mukherjee <pmukhe00@emea.mrshmc.com>
2019-02-21 14:01:13 +0000

    made changes to deploy on heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
-var port = process.env.port || 3000
+var port = process.env.PORT || 3000
 
 
 app.use(express.static(__dirname+'/public'));
